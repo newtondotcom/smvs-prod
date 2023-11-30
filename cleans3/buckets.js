374cfff02cd0e4612f776f34838ea656da373cff
newtondotcom <asphalt9fr@gmail.com>
2023-11-30 09:11:14 +0100

    separate buckets.js file for modularity

---
+++ cleans3/buckets.js
@@ -0,0 +1,18 @@
+const buckets = [
+    {
+        name : 'bucket1',
+        endpoint : 'play.min.io',
+        port : 9000,
+        accessKey : 'Q3AM3UQ867SPQQA43P2F',
+        secretKey : 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
+    },
+    {
+        name : 'bucket1',
+        endpoint : 'play.min.io',
+        port : 9000,
+        accessKey : 'Q3AM3UQ867SPQQA43P2F',
+        secretKey : 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'   
+    }
+]
+
+module.exports = buckets;
--- cleans3/index.js
+++ cleans3/index.js
@@ -1,4 +1,5 @@
 var Minio = require('minio')
+var buckets = require('./buckets')
 
 /*
 export default function generateUniqueName() {
@@ -11,23 +12,6 @@ export default function generateUniqueName() {
 */
 
 var minioClient 
-
-const buckets = [
-    {
-        name : 'bucket1',
-        endpoint : 'play.min.io',
-        port : 9000,
-        accessKey : 'Q3AM3UQ867SPQQA43P2F',
-        secretKey : 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'
-    },
-    {
-        name : 'bucket1',
-        endpoint : 'play.min.io',
-        port : 9000,
-        accessKey : 'Q3AM3UQ867SPQQA43P2F',
-        secretKey : 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'   
-    }
-]
 
 const dayBeforeDelete = 5;
 
