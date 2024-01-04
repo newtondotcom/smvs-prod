78c3da48ddf997df1d0fda9e336aac25b901c82d
newtondotcom <asphalt9fr@gmail.com>
2024-01-04 22:05:53 +0100

    weid docker container infinite loop

--- worker/app.py
+++ worker/app.py
@@ -43,17 +43,26 @@ def callback(ch, method, properties, body):
     local_file_path = "temp/"+file_name
     download_file(file_name, local_file_path,file_bucket_name)
 
+    print("File downloaded: "+local_file_path)
+
     #Process file
     path_in = local_file_path
     path_out = local_file_path.replace(".mp4","_out.mp4")
 
     trat_video(path_in,path_out,emoji,lsilence)
 
+    print("File processed: "+path_out)
+
     #Upload file to S3
     file_key = path_out
     upload_file(file_key.replace("temp/",""), file_key.replace("temp/",""),file_bucket_name)
 
+    print("File uploaded: "+file_key)
+
     remove_file(file_bucket_name,file_name)
+
+    print("File removed: "+file_name)
+
     #remove minia
     #remove_file(file_bucket_name,file_name.replace(".mp4",".png"))
 
@@ -66,8 +75,8 @@ def callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
     clean_temp()
+    
     print(" Done")
-    #exit()
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='task_queue', on_message_callback=callback)
