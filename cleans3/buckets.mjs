9bfe3777bea412e89b2ef08a3b31ff23bdba1b7f
newtondotcom <asphalt9fr@gmail.com>
2024-02-01 18:38:35 +0100

    list of minio buckets

---
+++ cleans3/buckets.mjs
@@ -0,0 +1,11 @@
+const buckets = [
+    {
+        name : "videos",
+        endpoint : '144.91.123.186',
+        port : 32771,
+        accessKey : 'oJTJnZIz0lJ8RblZMLbb',
+        secretKey : 'nyAeRaWm1vo9mBBwgKqhLzP1Yjws7V5IpVrfKPEe'
+    }
+]
+
+export default buckets;
