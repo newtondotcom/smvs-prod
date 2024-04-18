d7b584a1c7c5ac03fc0a81df09d7b965c1929fb7
newtondotcom <asphalt9fr@gmail.com>
2024-04-19 00:17:46 +0200

    introducing 3rd lowest model for testing

--- worker/gen.py
+++ worker/gen.py
@@ -5,11 +5,11 @@ device = "cpu"  # "cuda" or "cpu"
 batch_size = 8 # reduce if low on GPU mem, before 16
 compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy), before float16
 
-
-model = whisperx.load_model("medium", device, compute_type=compute_type)
+# medium model is "medium", large model is "large-v2"
+model = whisperx.load_model("base", device, compute_type=compute_type)
 
 def get_transcribe(audio_file):
-    # 1. Transcribe with original whisper (batched), medium model is "medium", large model is "large-v2"
+    # 1. Transcribe with original whisper (batched)
     audio = whisperx.load_audio(audio_file)
     result = model.transcribe(audio, batch_size=batch_size)
 
