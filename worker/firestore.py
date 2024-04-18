3ac8ceebdf6da4e487a2a5a7ae0b5a2df53c4c26
newtondotcom <asphalt9fr@gmail.com>
2024-04-19 00:20:41 +0200

    rm direct work with firestore because will use firebase cloud function or nuxt api

--- worker/firestore.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-default_app = firebase_admin.initialize_app()
-db = firestore.client()
-
-doc_ref = db.collection("users").document("abc")
-
--- worker/firestore.sh
+++
@@ -1,3 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/robin/Downloads/GITHUB/smvs-prod/worker/client_secrets.json"
-
-pip install --upgrade firebase-admin
