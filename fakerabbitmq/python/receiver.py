dc8494851a435a835e67164afecf77126d0833da
newtondotcom <asphalt9fr@gmail.com>
2024-04-19 00:12:00 +0200

    python server

---
+++ fakerabbitmq/python/receiver.py
@@ -0,0 +1,28 @@
+import pika
+import time
+import json
+
+print(' Connecting to server ...')
+
+try:
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="144.91.123.186",port=15672))
+except pika.exceptions.AMQPConnectionError as exc:
+    print("Failed to connect to RabbitMQ service. Message wont be sent.")
+    exit()
+    
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
+
+
+print(' Waiting for messages...')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    bodyjson = json.loads(body)
+    time.sleep(2)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print(" [x] Done")
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+channel.start_consuming()
