a7966b5b883c1d0de7eb6123db506ca6e0aab0f6
newtondotcom <asphalt9fr@gmail.com>
2024-04-20 12:22:11 +0200

    rename var

--- worker/emojis.py
+++ worker/emojis.py
@@ -11,19 +11,19 @@ def overlay_images_on_video(in_path, out_path, width, height,ass,emojis_list=Non
     swidth = (width-emoji_size)/2
     sheight = (height-emoji_size)/2 - y_offset
     filter_complex = ""
-    if image_list!=None :
-        image_list = [(path_emojis + image + ".png", start_time, end_time) for image, start_time, end_time in image_list]
-        for idx, (image_name, start_time, end_time) in enumerate(image_list):
+    if emojis_list!=None :
+        emojis_list = [(path_emojis + image + ".png", start_time, end_time) for image, start_time, end_time in emojis_list]
+        for idx, (image_name, start_time, end_time) in enumerate(emojis_list):
             previous_video = f"[{idx}v]" if idx > 0 else "[0:v]"
             filter_complex += f"{previous_video}[{idx + 1}:v]overlay={swidth}:{sheight}:enable='between(t,{start_time},{end_time})'"
-            if idx < len(image_list) - 1:
+            if idx < len(emojis_list) - 1:
                 filter_complex += f"[{idx + 1}v];"
             else:
                 filter_complex += f"[last];[last]ass='{ass}'[out]"
 
         # Build the complete ffmpeg command
         cmd = (
-            f"ffmpeg -i {in_path} {' '.join(['-i ' + image for image, _, _ in image_list])} "
+            f"ffmpeg -i {in_path} {' '.join(['-i ' + image for image, _, _ in emojis_list])} "
             f"-filter_complex \"{filter_complex}\" -map [out] -map 0:a -c:a copy {out_path} -y"
         )
 
