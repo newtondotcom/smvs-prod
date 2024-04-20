511d4133f2d2f99a74e4ea2cc73c0c280829ff50
newtondotcom <asphalt9fr@gmail.com>
2024-04-20 19:11:39 +0200

    changed input path

--- worker/dev.py
+++ worker/dev.py
@@ -17,7 +17,7 @@ def main():
     lsilence = False
 
     #Download file from S3
-    local_file_path = "../fakerabbitmq/python/temp/palma.mp4"
+    local_file_path = "../inputs/palma.mp4"
 
     #Process file
     path_in = local_file_path
