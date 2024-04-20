fcda79444e26c2ab38cde46a6a8fc28ec8592103
newtondotcom <asphalt9fr@gmail.com>
2024-04-20 18:51:06 +0200

    timer + dev pos : dont trascribe but use test_data file

--- worker/gen.py
+++ worker/gen.py
@@ -1,30 +1,42 @@
 import whisperx
 import gc 
+import time
+from testdata import datas
 
 device = "cpu"  # "cuda" or "cpu"
 batch_size = 8 # reduce if low on GPU mem, before 16
 compute_type = "int8" # change to "int8" if low on GPU mem (may reduce accuracy), before float16
 
 # medium model is "medium", large model is "large-v2"
+tic = time.perf_counter()
 model = whisperx.load_model("base", device, compute_type=compute_type)
+toc = time.perf_counter()
+print("Faster-Whisper model took ", toc - tic, " seconds to load")
 
 def get_transcribe(audio_file, align=False):
     # 1. Transcribe with original whisper (batched)
+    tic = time.perf_counter()
     audio = whisperx.load_audio(audio_file)
-    result = model.transcribe(audio, batch_size=batch_size)
+    tac = time.perf_counter()
+    print("Audio splitting took ",tac-tic," seconds")
 
-    # delete model if low on GPU resources
-    # import gc; gc.collect(); torch.cuda.empty_cache(); del model
+    #tic= time.perf_counter()
+    #result = model.transcribe(audio, batch_size=batch_size)
+    #toc = time.perf_counter()
+    #print("Transcription took ", toc - tic, " seconds")
 
-    if not align:
-        return result['segments']
+    #if align:
+        #return result['segments']
 
     # 2. Align whisper output
-    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-    result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+    model_a, metadata = whisperx.load_align_model(language_code="fr", device=device)
+    tic = time.perf_counter()
+    result = whisperx.align(datas, model_a, metadata, audio, device, return_char_alignments=False)
+    toc = time.perf_counter()
+    print("Alignment took ", toc - tic, " seconds")
 
-    # delete model if low on GPU resources
-    # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
+    # delete model if low on **GPU** resources
+    #gc.collect(); torch.cuda.empty_cache(); del model_a
     
     return result['segments']
 
