5c4a314354117d9c6831969b583323c4f1cc3e89
newtondotcom <asphalt9fr@gmail.com>
2024-04-22 00:42:43 +0200

    upate readme

--- build.sh
+++ build.sh
@@ -11,23 +11,23 @@ if [[ -z "$DOCKER_REGISTRY" || -z "$DOCKER_USERNAME" || -z "$DOCKER_PASSWORD" ||
     exit 1
 fi
 
-
-# Log in to the Docker registry
-echo "Logging in to Docker registry..."
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
-
 # Build the Docker image for the specified platform
 echo "Building Docker image for $TARGET_PLATFORM platform..."
 docker buildx create --use
 docker buildx build \
     --platform "$TARGET_PLATFORM" \
     -t "$IMAGE_WORKER_NAME:$IMAGE_WORKER_TAG" \
-    --secret id=.env,src=.env
-    worker/
+    --secret id=.env,src=.env \
+    worker/ \
+    --load
 
 # Tag the Docker image
 echo "Tagging Docker image..."
 docker tag "$IMAGE_WORKER_NAME:$IMAGE_WORKER_TAG" "$DOCKER_REGISTRY/$IMAGE_WORKER_NAME:$IMAGE_WORKER_TAG"
+
+# Log in to the Docker registry
+echo "Logging in to Docker registry..."
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
 
 # Push the Docker image to the registry
 echo "Pushing Docker image to registry..."
@@ -38,3 +38,5 @@ echo "Logging out of Docker registry..."
 docker logout "$DOCKER_REGISTRY"
 
 echo "Docker tasks completed successfully."
+
+## Make Server.js dockerfile
