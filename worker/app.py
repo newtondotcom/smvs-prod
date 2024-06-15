91e61f00e607d0e5d42a7ee0fe0e58b4cc8144f9
newtondotcom <asphalt9fr@gmail.com>
2024-06-15 12:17:13 +0200

    auto restart + dsicord log

--- worker/app.py
+++ worker/app.py
@@ -1,109 +1,125 @@
 #!/usr/bin/env python3
+import os
 import pika
+import json
+import requests
+import datetime
+import time
+from dotenv import load_dotenv
 from s3 import *
 from utils import *
 from gen import *
 from silent import *
-from s3 import *
 from emojis import *
 from processing import *
-import json
-from dotenv import load_dotenv
+
 load_dotenv()
 
-print(' Connecting to server ...')
+def main():
+    print(' Connecting to server ...')
 
-apikey = os.environ.get("API_KEY")
+    apikey = os.environ.get("API_KEY")
 
-RABBIT_HOST = os.environ.get("RABBIT_HOST")
-RABBIT_PORT = os.environ.get("RABBIT_PORT")
-try:
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_HOST,port=RABBIT_PORT))
-except pika.exceptions.AMQPConnectionError as exc:
-    print("Failed to connect to RabbitMQ service. Message wont be sent.")
-    exit()
-    
-channel = connection.channel()
-channel.queue_declare(queue='task_queue', durable=True)
+    RABBIT_HOST = os.environ.get("RABBIT_HOST")
+    RABBIT_PORT = os.environ.get("RABBIT_PORT")
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_HOST, port=RABBIT_PORT, heartbeat=600))
+    except pika.exceptions.AMQPConnectionError as exc:
+        print("Failed to connect to RabbitMQ service. Message won't be sent.")
+        exit()
+        
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
 
-thumbnails_bucket = "thumbnails2"
-s3_minia = S3(thumbnails_bucket)
+    thumbnails_bucket = "thumbnails2"
+    s3_minia = S3(thumbnails_bucket)
 
-print(' Waiting for messages...')
+    print(' Waiting for messages...')
 
-def callback(ch, method, properties, body):
-    global s3_minia
-    bodyjson = json.loads(body)
+    def callback(ch, method, properties, body):
+        global s3_minia
+        bodyjson = json.loads(body)
 
-    ## Parameters
-    file_name = bodyjson['file_name']
-    emoji = bodyjson['emoji']
-    lsilence = bodyjson['lsilence']
-    video_aligned = bodyjson['video_aligned']
-    key_db = bodyjson['key_db']
-    position = bodyjson['position']
+        ## Parameters
+        file_name = bodyjson['file_name']
+        emoji = bodyjson['emoji']
+        lsilence = bodyjson['lsilence']
+        video_aligned = bodyjson['video_aligned']
+        key_db = bodyjson['key_db']
+        position = bodyjson['position']
 
-    #S3_name = bodyjson['s3_name']
-    s3_uploads = S3("uploads")
-    S3_downloads = S3("downloads")
+        # S3_name = bodyjson['s3_name']
+        s3_uploads = S3("uploads")
+        S3_downloads = S3("downloads")
 
-    print("Trying to download file: "+file_name)
+        print("Trying to download file: " + file_name)
 
-    #Download file from S3
-    local_file_path = "temp/"+file_name
-    s3_uploads.download_file(file_name, local_file_path)
+        # Download file from S3
+        local_file_path = "temp/" + file_name
+        s3_uploads.download_file(file_name, local_file_path)
 
-    print("File downloaded: "+local_file_path)
+        print("File downloaded: " + local_file_path)
 
-    #Process file
-    path_in = local_file_path
-    path_out = local_file_path.replace(".mp4","_out.mp4")
+        # Process file
+        path_in = local_file_path
+        path_out = local_file_path.replace(".mp4", "_out.mp4")
 
-    time_encoding,time_transcription,time_alignment = process_video(path_in,path_out,emoji,lsilence,video_aligned,position)
+        time_encoding, time_transcription, time_alignment = process_video(path_in, path_out, emoji, lsilence, video_aligned, position)
 
-    print("File processed: "+path_out)
+        print("File processed: " + path_out)
 
-    #Upload video to videos S3
-    file_key = path_out
-    S3_downloads.upload_file(file_key)
+        # Upload video to videos S3
+        file_key = path_out
+        S3_downloads.upload_file(file_key)
 
-    print("File uploaded: "+file_key)
+        print("File uploaded: " + file_key)
 
-    s3_uploads.remove_file(file_name)
+        s3_uploads.remove_file(file_name)
 
-    print("File removed: "+file_name)
+        print("File removed: " + file_name)
 
-    #add minia with very low resolution    
-    thumbnail_path = path_in.replace(".mp4", ".jpg")
-    generate_thumbnail(path_in,thumbnail_path)
-    s3_minia.upload_file(thumbnail_path)
+        # add minia with very low resolution    
+        thumbnail_path = path_in.replace(".mp4", ".jpg")
+        generate_thumbnail(path_in, thumbnail_path)
+        s3_minia.upload_file(thumbnail_path)
 
-    print("Thumbnail uploaded: " + thumbnail_path.replace("temp/",""))
+        print("Thumbnail uploaded: " + thumbnail_path.replace("temp/", ""))
 
+        # Construction of the body for the frontend
+        body = {
+            "task_id": key_db,
+            "time_transcription": time_transcription,
+            "time_encoding": time_encoding,
+            "time_alignment": time_alignment,
+            "done_at": datetime.datetime.now().isoformat(),
+            "thumbnail": file_name.replace(".mp4", ".jpg")
+        }
+        headers = {'Authorization': 'Bearer ' + apikey}
+        requests.post("https://app.yogocap.com/api/dashboard/tasks", headers=headers, json=body)
 
-    # construction of the body for the frontend
-    body = {
-        "task_id": key_db,
-        "time_transcription": time_transcription,
-        "time_encoding": time_encoding,
-        "time_alignment": time_alignment,
-        "done_at": datetime.datetime.now().isoformat(),
-        "thumbnail": file_name.replace(".mp4", ".jpg")
-    }
-    headers = {'Authorization': 'Bearer '+apikey}
-    requests.post("https://app.yogocap.com/api/dashboard/tasks",headers=headers, json=body)
+        try:
+            clean_temporary_directory()
+        except OSError:
+            pass
+
+        # Advise server that file is ready
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        print(" Done")
 
     try:
-        clean_temporary_directory()
-    except OSError:
-        pass
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(queue='task_queue', on_message_callback=callback)
+        channel.start_consuming()
+    except Exception as e:
+        webhook = os.environ.get("DISCORD_WEBHOOK")
+        requests.post(webhook, json={"content": "Error in worker: " + str(e)})
+        raise
 
-
-    #Advice server that file is ready
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-    
-    print(" Done")
-
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
-channel.start_consuming()
+if __name__ == "__main__":
+    while True:
+        try:
+            main()
+        except Exception as e:
+            print(f"Error occurred: {e}. Restarting in 5 seconds...")
+            time.sleep(5)
