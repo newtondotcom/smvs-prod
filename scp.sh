29517e4c7c441ed23e43f1723eb648a2c1fbb6b0
newtondotcom <asphalt9fr@gmail.com>
2024-09-04 16:41:19 +0200

    chore : add scp command for the differnets root folders

--- scp.sh
+++ scp.sh
@@ -1,1 +1,3 @@
 scp -P 12839 -r ./worker/ ubuntu@158.178.200.181:/home/ubuntu
+# scp -P 12839 -r ./inputs/ ubuntu@158.178.200.181:/home/ubuntu
+# scp -P 12839 -r ./emojis/ ubuntu@158.178.200.181:/home/ubuntu
