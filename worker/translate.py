d9b14ca5901012df691003db78aef8c3231ffd02
newtondotcom <asphalt9fr@gmail.com>
2024-09-09 15:48:11 +0200

    build : back to inline translation for gh action

--- worker/translate.py
+++ worker/translate.py
@@ -1,15 +1,33 @@
-from libretranslatepy import LibreTranslateAPI
-import platform
+import argostranslate.package
+import argostranslate.translate
 
-url = "http://89.168.33.166:5000/"
-if platform.processor() == 'x86_64':
-    lt = LibreTranslateAPI(url)
-else:
-    lt = LibreTranslateAPI(url)
+from_code = "fr"
+to_code = "en"
+
+def check_package_installed(from_code,to_code):
+    langages = argostranslate.package.get_installed_packages()
+    for langage in langages:
+        if langage.from_code == from_code and langage.to_code == to_code:
+            return True
+
+def install_package(from_code,to_code):
+    argostranslate.package.update_package_index()
+    available_packages = argostranslate.package.get_available_packages()
+    package_to_install = next(
+        filter(
+            lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+        )
+    )
+    argostranslate.package.install_from_path(package_to_install.download())
 
 def translate(text,from_code):
     to_code = "en"
     if from_code != "en":
-        translatedText = lt.translate(text, from_code, to_code)
+        if not check_package_installed(from_code,to_code):
+            install_package(from_code,to_code)
+        translatedText = argostranslate.translate.translate(text, from_code, to_code)
         return translatedText
     return text
+
+
+    
